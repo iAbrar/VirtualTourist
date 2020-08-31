99ac9633e3e71dd915360a55f4d94490aad28f75
Abrar <abrar.ksu@gmail.com>
2020-08-31 13:02:46 +0300

    feat: Load images in collection view using static location

---
+++ VirtualTourist/Controllers/CollectionViewController.swift
@@ -0,0 +1,89 @@
+//
+//  CollectionViewController.swift
+//  VirtualTourist
+//
+//  Created by imac on 8/23/20.
+//  Copyright © 2020 Abrar. All rights reserved.
+//
+
+import UIKit
+import Kingfisher
+
+private let reuseIdentifier = "customCell"
+
+class CollectionViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    var imagesUrl = [String]()
+    
+    
+    @IBOutlet var collection: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collection.dataSource = self
+        collection.delegate = self
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let lat = "24.7136"
+        let lon = "46.6753"
+        
+        
+        FlickrAPI.downloadJSON(longitude: lat, latitude: lon, page: 1)
+        { images, error in
+            
+            
+            if error == nil
+            {
+                DispatchQueue.main.async {
+                    self.imagesUrl = images
+                    self.collection.reloadData()
+                    
+                }
+                
+            }
+            
+            print("willwill",self.imagesUrl.count)
+            
+        }
+        
+        
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using [segue destinationViewController].
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    // MARK: UICollectionViewDataSource
+    
+    
+     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return imagesUrl.count
+        
+    }
+    
+     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CollectionViewCell
+        
+        //        cell.imageView.image = UIImage(named: "y9DpT")
+        let url = URL(string: imagesUrl[indexPath.row])
+        
+        let image = UIImage(named: "y9DpT")
+        cell.imageView.kf.setImage(with: url, placeholder: image)
+        
+        
+        return cell
+    }
+    
+
+    
+}
--- VirtualTourist/Controllers/ViewController.swift
+++ VirtualTourist/Controllers/ViewController.swift
@@ -11,18 +11,14 @@ import MapKit
 
 class ViewController: UIViewController {
 
-    var apiManager = APImanager()
+//    var apiManager = APImanager()
     var page = 1
     
     @IBOutlet weak var mapView: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let lat = "24.7136"
-        let lon = "46.6753"
-        
-        apiManager.fetchImages(longitude: lon, latitude: lat, page: page)
+
     }
 
 
