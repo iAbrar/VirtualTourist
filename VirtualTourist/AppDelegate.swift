0ba6daa2e5ee58638369bc3f74fc424cd3c1e6f9
Abrar <abrar.ksu@gmail.com>
2020-09-01 18:13:58 +0300

    feat: Complete the project

--- VirtualTourist/AppDelegate.swift
+++ VirtualTourist/AppDelegate.swift
@@ -17,6 +17,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let navigationController = window?.rootViewController as! UINavigationController
+        let mapViewController = navigationController.topViewController as! ViewController
+        mapViewController.dataController = dataController
         dataController.load()
         
         return true
@@ -30,6 +33,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        saveViewContext()
+
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -42,8 +47,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        saveViewContext()
+
     }
 
+    func saveViewContext() {
+        try? dataController.viewContext.save()
+    }
 
 }
 
--- VirtualTourist/Controllers/CollectionViewController.swift
+++ VirtualTourist/Controllers/CollectionViewController.swift
@@ -9,14 +9,18 @@
 import UIKit
 import Kingfisher
 import MapKit
+import CoreData
 
 
 private let reuseIdentifier = "customCell"
 
-class CollectionViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class CollectionViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, MKMapViewDelegate {
     
-    var imagesUrl = [String]()
-    var pin : MKAnnotation?
+//    var photos: [Photo] = []
+    var pin : Pin!
+    var dataController: DataController!
+    var fetchedResultsController:NSFetchedResultsController<Photo>!
+    
     var page = 1
     
     @IBOutlet var collection: UICollectionView!
@@ -24,63 +28,81 @@ class CollectionViewController: UIViewController, UICollectionViewDelegate, UICo
     @IBOutlet weak var mapView: MKMapView!
     
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collection.dataSource = self
         collection.delegate = self
+        setupFetchedResultsController()
+
+        let annotation = MKPointAnnotation()
         
-        mapView.addAnnotation(pin!)
-        mapView.showAnnotations([pin!], animated: true)
+        annotation.coordinate = CLLocationCoordinate2D(latitude:pin.latitude, longitude: pin.longitude)
+        
+        mapView.addAnnotation(annotation)
+        
     }
+    
+    
     
     override func viewWillAppear(_ animated: Bool) {
-       
-        guard  let lat = pin?.coordinate.latitude else { return }
-        guard  let lon = pin?.coordinate.longitude else { return }
-
+        
+        guard  let lat = pin?.latitude else { return }
+        guard  let lon = pin?.longitude else { return }
+        
         page = 1
-        
-        FlickrAPI.downloadJSON(longitude: lon, latitude: lat, page: page)
-        { images, error in
-            
-            
-            if error == nil
-            {
-                DispatchQueue.main.async {
-                    self.imagesUrl = images
-                    self.collection.reloadData()
-                    
-                }
-                
-            }
-            
-        }
-    }
-    
-    
-     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        setupFetchedResultsController()
 
-        return imagesUrl.count
-        
+        if (fetchedResultsController.sections?[0].numberOfObjects ?? 0 == 0) {
+        loadImages(lon, lat, page)
+        }
+        collection.reloadData()
     }
     
-     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CollectionViewCell
-        
-        //        cell.imageView.image = UIImage(named: "y9DpT")
-        let url = URL(string: imagesUrl[indexPath.row])
-        
+        let photo = fetchedResultsController.object(at: indexPath)
         let image = UIImage(named: "placeholder")
-        cell.imageView.kf.setImage(with: url, placeholder: image)
+        
+        if let data = photo.data {
+            cell.imageView.image = UIImage(data: data)
+        } else if let url = photo.url {
+            guard let imageUrl = URL(string: url) else{
+                return cell
+            }
+        
+        
+        cell.imageView.kf.setImage(with: imageUrl, placeholder: image, options: nil, progressBlock: nil) { (imge, error, cacheType, url) in
+                if ((error) != nil) {
+                    
+                } else {
+                    photo.data = imge?.pngData()
+                    try? self.dataController.viewContext.save()
+                }
+            }
+        }
         
         
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        _ = self.imagesUrl.remove(at:indexPath.row)
-        self.collection.deleteItems(at: [indexPath])
+        let photo = fetchedResultsController.object(at: indexPath)
+        dataController.viewContext.delete(photo)
+        do {
+            try dataController.viewContext.save()
+        } catch {
+            print("error")
+        }
         print("delete")
         
     }
@@ -88,23 +110,73 @@ class CollectionViewController: UIViewController, UICollectionViewDelegate, UICo
     @IBAction func newCollectionTapped(_ sender: UIButton) {
         
         page = page+1
-        print(page)
         
-        FlickrAPI.downloadJSON(longitude: pin!.coordinate.longitude, latitude: pin!.coordinate.latitude, page: page)
-            { images, error in
-                
-                
-                if error == nil
-                {
-                    DispatchQueue.main.async {
-                        self.imagesUrl = images
-                        self.collection.reloadData()
-                        
+        loadImages(pin!.longitude, pin!.latitude, page)
+         collection.reloadData()
+    }
+    
+    fileprivate func loadImages(_ lon: Double, _ lat: Double, _ page: Int) {
+        FlickrAPI.downloadJSON(longitude: lon, latitude: lat, page: page)
+        { images, error in
+            
+            
+            if error == nil
+            {
+                DispatchQueue.main.async {
+                    
+                    for image in images
+                    {
+                        let photo = Photo(context: self.dataController.viewContext)
+                        photo.creationDate = Date()
+                        photo.url = image
+                        photo.pin = self.pin
+                        try? self.dataController.viewContext.save()
                     }
+                    self.collection.reloadData()
                     
                 }
-                
+              
+            }
+            
         }
     }
     
+    fileprivate func setupFetchedResultsController() {
+        let fetchRequest:NSFetchRequest<Photo> = Photo.fetchRequest()
+        let predicate = NSPredicate(format: "pin == %@", pin)
+        fetchRequest.predicate = predicate
+        let sortDescriptor = NSSortDescriptor(key: "creationDate", ascending: true)
+        fetchRequest.sortDescriptors = [sortDescriptor]
+        
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest, managedObjectContext: dataController.viewContext, sectionNameKeyPath: nil, cacheName: "\(pin)-photos")
+        fetchedResultsController.delegate = self
+        
+        do {
+            try fetchedResultsController.performFetch()
+        } catch {
+            fatalError("The fetch could not be performed: \(error.localizedDescription)")
+        }
+    }
+    
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+    
+    // center the mapView on the selected pin
+    let region = MKCoordinateRegion(center: view.annotation!.coordinate, span: mapView.region.span)
+    mapView.setRegion(region, animated: true)
+    }
+    
+}
+
+extension CollectionViewController: NSFetchedResultsControllerDelegate {
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        switch type {
+        case .insert:
+            collection.insertItems(at: [newIndexPath!])
+            break
+        case .delete:
+            collection.deleteItems(at: [indexPath!])
+            break
+        default: ()
+        }
+    }
 }
